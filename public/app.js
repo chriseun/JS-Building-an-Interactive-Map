392b50b07569d8b31e494fc1a41820e2508d16d9
Chris Eun <eunchristopher@gmail.com>
2022-12-08 10:01:26 -0800

    Building interactive map example using JS and Leaflet

--- public/app.js
+++ public/app.js
@@ -39,16 +39,20 @@ polygon.addTo(myMap)
 
 
 // create red pin marker
+var redPin = L.icon({
+    iconUrl: 'assets/red-pin.png',
 
+    iconSize: [38, 38]
+})
 // const rSOne = L.marker([48.866200610611926, 2.352236247419453],{icon: redPin}).bindPopup('RÃ©aumur-SÃ©bastopol')
 
 // metro station markers
 // Metro station markers:
-const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('RÃ©aumur-SÃ©bastopol')
-const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
-const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
-const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
-const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
-const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
+const rS = L.marker([48.866200610611926, 2.352236247419453],{icon: redPin}).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196],{icon: redPin}).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739],{icon: redPin}).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167],{icon: redPin}).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543],{icon: redPin}).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714],{icon: redPin}).bindPopup('Grands Boulevards')
 
 const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
